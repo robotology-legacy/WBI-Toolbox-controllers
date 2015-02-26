93ae5f248160fae4968dfcaff8b9cf54c07978c4
Daniele <daniele.pucci@iit.it>
2015-02-26 18:08:34 +0100

    Still cleaning up impedance control

--- controllers/wholeBodyImpedance/init_impedanceControl.m
+++ controllers/wholeBodyImpedance/init_impedanceControl.m
@@ -1,28 +1,11 @@
-%% DoublePendulum
-% setenv('YARP_ROBOT_NAME', 'double_pendulum');
-% ROBOT_DOF = 2;
-% robotName = 'doublePendulumGazebo';
-% localName = 'impedance';
-% Ts        = 0.01;
-% Kp        = 1.0;
-% Kd        = 1.0;
-% ROBOT_DOF = 2;
-% robotName = 'doublePendulumGazebo';
-% localName = 'impedance';
-% Ts        = 0.01;
-% Kp        = 1.0;
-% Kd        = 1.0;
-
 %% icubGazeboSim
 % setenv('YARP_ROBOT_NAME', 'icubGazeboSim');
 
-
-ROBOT_DOF = 19;
+ROBOT_DOF = 1;
 Kp        = 1/10*diag( ones(1,ROBOT_DOF)  );
 
 
 robotName = 'icub';
 localName = 'impedance';
 Ts        = 0.01;
-% Kp        = 0.01;
 Kd        = 0.0001;
