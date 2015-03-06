553cb13b4de9d89b0e97c4f9f3d95e6bad6d5403
Daniele <daniele.pucci@iit.it>
2015-03-06 15:56:53 +0100

    Modified home configuration of balancing on two feet for iCubGenova03

--- controllers/balancing_with_opt_of_tau/robots/iCubGenova03/gains.m
+++ controllers/balancing_with_opt_of_tau/robots/iCubGenova03/gains.m
@@ -1,8 +1,10 @@
-directionOfOscillation            = [0;1;0];
-referenceParams                   = [0.0  0.0];  %referenceParams(1) = amplitude of ascillations in meters referenceParams(2) = frequency of ascillations in hertz
+ROBOT_DOF = 23;
+
 amplitudesOscillationsOnOneFoot   = zeros(1,ROBOT_DOF);
 frequenciesOscillationsOnOneFoot  = zeros(1,ROBOT_DOF);
-noOscillationTime = 0;
+directionOfOscillation            = [0;1;0];
+referenceParams                   = [0.0  0.0];  %referenceParams(1) = amplitude of ascillations in meters referenceParams(2) = frequency of ascillations in hertz
+
 ROBOT_DOF_FOR_SIMULINK = eye(ROBOT_DOF);
 % Controller gains for convergence of the desired centroidal momentum. 
 % The first three elements are the Proportional, Intagral, and the Derivative
@@ -110,3 +112,7 @@ end
 impedances          = [impTorso(1,:),impArms(1,:),impArms(1,:),impLeftLeg(1,:),impRightLeg(1,:)];
 increasingRatesImp  = [impTorso(2,:),impArms(2,:),impArms(2,:),impLeftLeg(2,:),impRightLeg(2,:)];
 impedencesSat       = [80   100    400];
+
+if (size(impedances,2) ~= ROBOT_DOF)
+    error('Dimension mismatch between ROBOT_DOF and dimension of the variable impedences. Check these variables in the file gains.m');
+end
