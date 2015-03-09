0c710cfacf391d507b25b7e76b8b0ebb153c1c63
Daniele <daniele.pucci@iit.it>
2015-03-09 09:10:39 +0100

    Fix in init file

--- controllers/balancing_with_opt_of_tau/initBalancingController_with_opt_of_tau.m
+++ controllers/balancing_with_opt_of_tau/initBalancingController_with_opt_of_tau.m
@@ -1,23 +1,20 @@
 clear all
- 
-YARP_ROBOT_NAME = getenv('YARP_ROBOT_NAME');
 
 robotName = 'icub';
 localName = 'balancing';
-ROBOT_DOF = 23;
 
-number_of_feet_on_ground          = 2;
-DEMO_LEFT_AND_RIGHT               = 1; 
+number_of_feet_on_ground = 2;
+DEMO_LEFT_AND_RIGHT      = 0;
+noOscillationTime        = 0;
 
-DEMO_MOVING_LEG_AND_ARMS = 0;  % It is taken into account only if number_of_feet_on_ground = 1
 
+DEMO_MOVING_LEG_AND_ARMS = 0;  
 
 % Controller period
 Ts                = 0.01;
 simulationTime    = inf;
 
-run(strcat('robots/',YARP_ROBOT_NAME,'/gains.m')); %Load gains for the specific robot
+% Load gains and parameters for the specific robot
+run(strcat('robots/',getenv('YARP_ROBOT_NAME'),'/gains.m')); 
 
-if (size(impedances,2) ~= ROBOT_DOF)
-    error('Dimension mismatch between ROBOT_DOF and dimension of the variable impedences. Check these variables in the file gains.m');
-end
+% run(strcat('robots/','iCubHeidelberg01','/gains.m')); 
