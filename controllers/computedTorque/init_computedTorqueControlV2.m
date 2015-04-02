9daef83036ecf9eb17eb5e20951ecebbe8840cdd
Pucci <pucci@IITRBCSWS131>
2015-04-02 08:53:45 +0200

    Tuning for iCubGenova03

--- controllers/computedTorque/init_computedTorqueControlV2.m
+++ controllers/computedTorque/init_computedTorqueControlV2.m
@@ -1,11 +1,10 @@
 %% icubGazeboSim
 
-ROBOT_DOF = 3;
-Kp        = 260*diag( ones(1,ROBOT_DOF)  );
+ROBOT_DOF = 1;
+Kp        = 250*diag( ones(1,ROBOT_DOF)  );
 
 ampl_and_freq = [ 20     % Amplitudes of ascillations for reference signals
                   0.4];  % Frequencies of oscillations for reference signals
-              
               
 simulationTime = 5/ampl_and_freq(2);               
 % COnversion into radiants and rad/s
