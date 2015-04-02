116184419cd5deb707ecf4a4517b1a19b6a783b7
Pucci <pucci@IITRBCSWS131>
2015-04-02 14:16:13 +0200

    corrected robotName

--- controllers/balancing_with_opt_of_tau/initBalancingController_with_opt_of_tau.m
+++ controllers/balancing_with_opt_of_tau/initBalancingController_with_opt_of_tau.m
@@ -5,9 +5,9 @@ localName = 'balancing';
 
 simulationTime    = inf;       % Simulation time in seconds
 
-number_of_feet_on_ground = 1;  % Either 1 or 2
+number_of_feet_on_ground = 2;  % Either 1 or 2
 
-DEMO_LEFT_AND_RIGHT      = 1;  % Either 0 or 1 
+DEMO_LEFT_AND_RIGHT      = 0;  % Either 0 or 1 
 noOscillationTime        = 10; % If DEMO_LEFT_AND_RIGHT = 1, the variable noOscillationTime is the time, in seconds, 
                                % that the robot waits before starting the left-and-righ
 
