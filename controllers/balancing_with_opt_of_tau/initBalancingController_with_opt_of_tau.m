98425348eb6308f4cbb03dfb768d495f09b822ff
Daniele <daniele.pucci@iit.it>
2015-04-03 16:28:50 +0200

    Some small fixes

--- controllers/balancing_with_opt_of_tau/initBalancingController_with_opt_of_tau.m
+++ controllers/balancing_with_opt_of_tau/initBalancingController_with_opt_of_tau.m
@@ -1,13 +1,13 @@
 clear all;
 
-robotName = 'icub';            
+robotName = 'icubGazeboSim';            
 localName = 'balancing';
 
 simulationTime    = inf;       % Simulation time in seconds
 
 number_of_feet_on_ground = 2;  % Either 1 or 2
 
-DEMO_LEFT_AND_RIGHT      = 0;  % Either 0 or 1 
+DEMO_LEFT_AND_RIGHT      = 1;  % Either 0 or 1 
 noOscillationTime        = 10; % If DEMO_LEFT_AND_RIGHT = 1, the variable noOscillationTime is the time, in seconds, 
                                % that the robot waits before starting the left-and-righ
 
