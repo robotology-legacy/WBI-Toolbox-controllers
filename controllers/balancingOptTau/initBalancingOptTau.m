02f548f728309d67bd800fd71e747532c7df7e13
Daniele <daniele.pucci@iit.it>
2015-04-10 15:12:51 +0200

    Some tuning for iCupParis01

--- controllers/balancingOptTau/initBalancingOptTau.m
+++ controllers/balancingOptTau/initBalancingOptTau.m
@@ -1,11 +1,11 @@
 clear all;
 
-robotName = 'icub';            
+robotName = 'icubGazeboSim';            
 localName = 'balancing';
 
 simulationTime    = inf;       % Simulation time in seconds
 
-number_of_feet_on_ground = 1;  % Either 1 or 2
+number_of_feet_on_ground = 2;  % Either 1 or 2
 
 DEMO_LEFT_AND_RIGHT      = 0;  % Either 0 or 1 
 noOscillationTime        = 0; % If DEMO_LEFT_AND_RIGHT = 1, the variable noOscillationTime is the time, in seconds, 
