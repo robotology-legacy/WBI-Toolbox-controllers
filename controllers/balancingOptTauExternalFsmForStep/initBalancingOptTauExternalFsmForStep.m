deb18031891e64d7d640067b7130dfbc3f608b3e
Daniele <daniele.pucci@iit.it>
2015-04-10 09:16:33 +0200

    Added controller for balancing and step with external coordinator

---
+++ controllers/balancingOptTauExternalFsmForStep/initBalancingOptTauExternalFsmForStep.m
@@ -0,0 +1,13 @@
+clear all
+
+robotName = 'icub';
+localName = 'balancing';
+
+
+% Controller period
+Ts                = 0.01;
+simulationTime    = inf;
+
+% Load gains and parameters for the specific robot
+run(strcat('robots/',getenv('YARP_ROBOT_NAME'),'/gains.m')); 
+
