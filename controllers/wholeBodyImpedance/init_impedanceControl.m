fb6636dc026a1c50ffe521a82aff2b8b35f4295f
Daniele <daniele.pucci@iit.it>
2015-07-29 18:46:08 +0200

    [WBI-Toolbox-controllers/wholeBodyImpendance] Added computed torque option

--- controllers/wholeBodyImpedance/init_impedanceControl.m
+++ controllers/wholeBodyImpedance/init_impedanceControl.m
@@ -3,20 +3,25 @@ ROBOT_DOF = 1;
 robotName = 'icub';
 localName = 'impedance';
 Ts        = 0.01;
-simulationTime = inf;
 
 KpTorso   = 0.5*ones(1,3);
 KpArms    = [0.2,0.2,0.2,0.05,0.1];
 KpLegs    = [0.2,0.2,0.2,0.05,0.1,0.1];
-Kp        = diag([0.1]);
+Kp        = diag([1]);
 
 if size(Kp,1) ~= ROBOT_DOF
     error('Dimension of Kp different from ROBOT_DOF')
 end
 
-Kd        = 0.0001;
+Kd        = 2*sqrt(Kp)*0;
 
 GRAV_COMP = 1;
-MOVING    = 0;
+MOVING    = 1;
 AMPLS     = 10*ones(1,ROBOT_DOF);
-FREQS     = 1/1*ones(1,ROBOT_DOF);
+FREQS     = 0.5*ones(1,ROBOT_DOF);
+
+if MOVING == 1
+    simulationTime = 5/FREQS(1);
+else
+    simulationTime = inf;
+end
