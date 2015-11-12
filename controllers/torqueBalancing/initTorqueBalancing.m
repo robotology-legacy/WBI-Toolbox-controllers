9be3cdd5dc9b61b245ccf9ecd01077238e22d718
FirstName <daniele.pucci85@gmail.com>
2015-11-12 01:14:29 +0100

    Cleaned FSM and internal coordinator

--- controllers/torqueBalancing/initTorqueBalancing.m
+++ controllers/torqueBalancing/initTorqueBalancing.m
@@ -1,45 +1,49 @@
-% clear all;
-clear;
+clear all;
 clc;
 
 robotName = 'icub';            
 localName = 'matlabTorqueBalancing';
 
-simulationTime    = inf;       % Simulation time in seconds
+simulationTime    = inf;    % Simulation time in seconds
 
 USE_QP_SOLVER    = 0;
 
 LEFT_RIGHT_FOOT_IN_CONTACT  = [1 0];
 
-DEMO_MOVEMENTS      = 1;  % Either 0 or 1 
+DEMO_MOVEMENTS      = true;% Either true or false 
 
-SMOOTH_DES_COM      = 0;   % If equal to one, the desired streamed values of the center of mass are smoothed internally through mimum jerk trajectory
-SMOOTH_DES_Q        = 1;   % If equal to one, the desired streamed values of the postural tasks are smoothed internally through mimum jerk trajectory
-SMOOTH_CONSTRAINTS  = 1;   % If equal to one, the desired streamed values of the logic vector identifying the active constraints are smoothed internally through mimum jerk trajectory
+SMOOTH_DES_COM      = 0;    % If equal to one, the desired streamed values 
+                            % of the center of mass are smoothed internally 
+SMOOTH_DES_Q        = 1;    % If equal to one, the desired streamed values 
+                            % of the postural tasks are smoothed internally 
+SMOOTH_CONSTRAINTS  = 1;    % If equal to one, the desired streamed values 
+                            % of the logic vector identifying the active 
+                            % constraints are smoothed internally 
 
-USE_SM = true; % If equal to true, the (internal) state machine will be used. The robot will switch from 2 to 1 (left) foot
-               % PLEASE, use logical values (true or false)
-% Controller period
-Ts                = 0.01; % [s]
+% PLEASE, use logical values (true or false) for the following variable
 
-% Load gains and parameters for the specific robot
+USE_SM              = false; % If equal to true, the (internal) state machine 
+                            % will be used. The robot will switch from 2 feet 
+                            % to 1 (left) foot
+
+                           
+Ts                  = 0.01; %  Controller period [s]
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CONFIGURATIONS COMPLETED: loading gains and parameters for the specific robot
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 run(strcat('robots/',getenv('YARP_ROBOT_NAME'),'/gains.m')); 
 addpath('extra/')
 [ConstraintsMatrix,bVectorConstraints]= constraints(forceFrictionCoefficient,numberOfPoints,torsionalFrictionCoefficient,gain.footSize,fZmin);
 
-if DEMO_MOVEMENTS == 1
-    robotSpecificReferences = fullfile('robots',getenv('YARP_ROBOT_NAME'),'initReferences.m');
-    if exist(robotSpecificReferences, 'file')
-        run(robotSpecificReferences);
-    else
-        %just try to launch the one in the path (?)
-        initReferences;
-    end
-end
 
-if USE_SM == true
-    initStateMachine;
-end
+robotSpecificReferences = fullfile('robots',getenv('YARP_ROBOT_NAME'),'initRefGen.m');
+run(robotSpecificReferences);
+robotSpecificFSM = fullfile('robots',getenv('YARP_ROBOT_NAME'),'initStateMachine.m');
+run(robotSpecificFSM);
+
 
 % If you want to sync Gazebo and simulink, 
 % remember that you have to launch gazebo as follow:
