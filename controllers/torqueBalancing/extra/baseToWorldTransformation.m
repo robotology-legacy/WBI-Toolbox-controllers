cccbad51d3389777912b31b8a0960b9f2e740414
FirstName <daniele.pucci85@gmail.com>
2015-11-16 12:28:00 +0100

    Bug fix in state machine

--- controllers/torqueBalancing/extra/baseToWorldTransformation.m
+++ controllers/torqueBalancing/extra/baseToWorldTransformation.m
@@ -9,6 +9,6 @@ function w_H_b_updated = baseToWorldTransformation(poseLeftFoot,poseRightFoot,w_
 
     r_H_b         = w_H_r\w_H_b;
 
-    w_H_b_updated = w_H_lr*(constraints(1)*l_H_b + constraints(2)*r_H_b );
+    w_H_b_updated = w_H_lr*(constraints(1)*l_H_b + constraints(2)*(1-constraints(1))*r_H_b );
     
 end
