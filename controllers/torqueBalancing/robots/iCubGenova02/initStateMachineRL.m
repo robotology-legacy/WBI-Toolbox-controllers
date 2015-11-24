e2e74ddc8978fa00609c7e3416dc08ca7a3a9455
FirstName <daniele.pucci85@gmail.com>
2015-11-24 19:02:24 +0100

    Added configuration file for transition to right

--- controllers/torqueBalancing/robots/iCubGenova02/initStateMachineRL.m
+++ controllers/torqueBalancing/robots/iCubGenova02/initStateMachineRL.m
@@ -27,36 +27,36 @@ if USE_SM
 
 
     %                   %   TORSO  %%      LEFT ARM   %%      RIGHT ARM   %%         LEFT LEG            %%         RIGHT LEG           %% 
-    gain.impedances  = [10   10   20, 10   10    10    8, 10   10    10    8, 30   30   20    20     10  10, 30   50   30    60      5   5  % state == 1  TWO FEET BALANCING
-                        10   10   20, 10   10    10    8, 10   10    10    8, 30   30   20    20     10  10, 30   50   30    60      5   5  % state == 2  COM TRANSITION TO LEFT 
-                        10   10   20, 10   10    10    8, 10   10    10    8, 30   30   20    20     10  10, 30   50   30    60      5   5  % state == 3  LEFT FOOT BALANCING
-                        30   30   30, 10   10    20   10, 10   10    20   10,100   90   20    20     10  10,100   50   30   100     25  25  % state == 4  YOGA LEFT FOOT 
-                        30   30   30, 10   10    20   10, 10   10    20   10,200  250   20    20     10  10,220  550  220   200     65 300  % state == 5  PREPARING FOR SWITCHING 
-                        30   30   30, 10   10    20   10, 10   10    20   10,100  350   20   200     10 100,220  550  220   200     65 300  % state == 6  LOOKING FOR CONTACT
+    gain.impedances  = [10   10   20, 10   10    10    8, 10   10    10    8, 30   50   30    60      5   5, 30   30   20    20     10  10  % state == 1  TWO FEET BALANCING
+                        10   10   20, 10   10    10    8, 10   10    10    8, 30   50   30    60      5   5, 30   30   20    20     10  10  % state == 2  COM TRANSITION TO LEFT 
+                        10   10   20, 10   10    10    8, 10   10    10    8, 30   50   30    60      5   5, 30   30   20    20     10  10  % state == 3  LEFT FOOT BALANCING
+                        30   30   30, 10   10    20   10, 10   10    20   10,100   50   30   100     25  25,100   90   20    20     10  10  % state == 4  YOGA LEFT FOOT 
+                        30   30   30, 10   10    20   10, 10   10    20   10,220  550  220   200     65 300,200  250   20    20     10  10  % state == 5  PREPARING FOR SWITCHING 
+                        30   30   30, 10   10    20   10, 10   10    20   10,220  550  220   200     65 300,100  350   20   200     10 100  % state == 6  LOOKING FOR CONTACT
                         30   50   30, 10   10    20   10, 10   10    20   10, 30   30   20    20     10  10, 30   30   20    20     10  10];% state == 7  BALANCING TWO FEET 
 
 end              
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                      
          
 %% %%%%%%%%%%%%%%%%    FINITE STATE MACHINE SPECIFIC PARAMETERS
-sm.com.threshold       = 0.005;
-sm.wrench.threshold    = 25;
-sm.joints.thresholdRL  = 3;
-sm.joints.thresholdLL  = 3;
+sm.com.threshold          =  0.005;
+sm.wrench.threshold       = 25;
+sm.joints.thresholdRL     =  3;
+sm.joints.thresholdLL     =  3;
 
-sm.stateAt0               = 1;
+sm.stateAt0               =  1;
 
-sm.DT                     = 1;
+sm.DT                     =  1;
 sm.waitingTimeAfterYoga   = 20;
 
 
 
-sm.com.states      = [0.0,  0.01,0.511;   %% CoM reference state 1
-                      0.0,  0.00,0.511;   %% CoM reference state 2
-                      0.0,  0.01,0.511;   %% CoM reference state 3
-                      0.0,  0.01,0.511;   %% CoM reference state 4
-                      0.0,  0.01,0.511;   %% CoM reference state 5
-                      0.0, -0.09,0.511    %% CoM reference state 6
+sm.com.states      = [0.0, -0.14,0.511;   %% CoM reference state 1
+                      0.0, -0.04,0.511;   %% CoM reference state 2
+                      0.0, -0.14,0.511;   %% CoM reference state 3
+                      0.0, -0.14,0.511;   %% CoM reference state 4
+                      0.0, -0.14,0.511;   %% CoM reference state 5
+                      0.0, -0.09,0.511;   %% CoM reference state 6
                       0.0, -0.09,0.511];  %% CoM reference state 7
 
 sm.tBalancing      = 0;
@@ -70,28 +70,28 @@ sm.joints.states = [[0.0864,0.0258,0.0152, ...                          %% state
                     [-0.0348,0.0779,0.0429, ...                         %% state == 2  COM TRANSITION TO LEFT 
                      -0.1493,0.8580,0.2437,0.8710 ...                   %
                      -0.1493,0.8580,0.2437,0.8710 ...                   %
-                     -0.0015,-0.1109,-0.0001,0.0003,0.0160,0.1630, ...  %  
-                     0.0005,0.0793,-0.0014,-0.0051,0.0073,-0.1151];     %  
+                     0.0005,0.0793,-0.0014,-0.0051,0.0073,-0.1151, ...  %  
+                     -0.0015,-0.1109,-0.0001,0.0003,0.0160,0.1630];     %  
                     [0.0864,0.0258,0.0152, ...                          %% state == 3  LEFT FOOT BALANCING
                      0.1253,0.8135,0.3051,0.7928 ...                    %    
                      0.0563,0.6789,0.3340,0.6214 ...                    %
-                     -0.0015,-0.1109,-0.0001,0.0003,0.0160,0.1630, ...  %  
-                     0.0005,0.0793,-0.0014,-0.0051,0.0073,-0.1151];     % 
+                     0.0005,0.0793,-0.0014,-0.0051,0.0073,-0.1151, ...  %  
+                     -0.0015,-0.1109,-0.0001,0.0003,0.0160,0.1630];     %  
                     [0.0864,0.0258,0.0152, ...                          %% state == 4  YOGA LEFT FOOT, THIS REFERENCE IS IGNORED 
                      0.1253,0.8135,0.3051,0.7928 ...                    %
                      0.0563,0.6789,0.3340,0.6214 ...                    %
-                     0.0522,-0.2582,0.0014,-0.2129,-0.0944,0.1937,...   %
-                     0.0128,0.4367,0.0093,-0.1585,-0.0725,-0.2931];     %
+                     0.0005,0.0793,-0.0014,-0.0051,0.0073,-0.1151, ...  %  
+                     -0.0015,-0.1109,-0.0001,0.0003,0.0160,0.1630];     %  
                     [-0.0348,0.0779,0.0429, ...                         %% state == 5  PREPARING FOR SWITCHING
                      -0.1493,0.8580,0.2437,0.8710 ...                   %
                      -0.1493,0.8580,0.2437,0.8710 ...                   %
-                     -0.0015,-0.1109,-0.0001,0.0003,0.0160,0.1630, ...  %  
-                     0.0005,0.0793,-0.0014,-0.0051    0.0073   -0.1151];%                                  %
+                      0.0005,0.0793,-0.0014,-0.0051,0.0073,-0.1151, ... %  
+                      -0.0015,-0.1109,-0.0001,0.0003,0.0160,0.1630];    %                                  %
                     [0.0864,0.0258,0.0152, ...                          %% state == 6  LOOKING FOR CONTACT
                      0.1253,0.8135,0.3051,0.7928 ...                    %
                      0.0563,0.6789,0.3340,0.6214 ...                    %
-                     0.0107,-0.0741,-0.0001,-0.0120,0.0252,0.1369,...   %
-                     -0.0026,0.0225,0.0093,-0.0020,0.0027,-0.0277];     %   
+                     -0.0026,0.0225,0.0093,-0.0020,0.0027,-0.0277,...   %
+                     0.0107,-0.0741,-0.0001,-0.0120,0.0252,0.1369];     %   
                     [0.0864,0.0258,0.0152, ...                          %% state == 7  BALANCING TWO FEET, THIS REFERENCE IS IGNORED 
                      0.1253,0.8135,0.3051,0.7928 ...                    %
                      0.0563,0.6789,0.3340,0.6214 ...                    %
@@ -155,6 +155,18 @@ sm.joints.points = [ 0,                                q1;
                      5*references.joints.smoothingTime,q6
                      6*references.joints.smoothingTime,q7
                      7*references.joints.smoothingTime,q8];
+					 
+for i = 1:size(sm.joints.points,1)				
+	sm.joints.points(i,2:4)          = [sm.joints.points(i,2) -sm.joints.points(i,3) -sm.joints.points(i,4)];
+	
+	rightArm                          =  sm.joints.points(i,end-15:end-12);
+	sm.joints.points(i,end-15:end-12) =  sm.joints.points(i,end-19:end-16);
+	sm.joints.points(i,end-19:end-16) =  rightArm;
+	
+	rightLeg                          =  sm.joints.points(i,end-5:end);
+	sm.joints.points(i,end-5:end)     =  sm.joints.points(i,end-11:end-6);
+	sm.joints.points(i,end-11:end-6)  =  rightLeg;
+end	 
                  
 % sm.joints.points = [ references.joints.smoothingTime,q9];
 
