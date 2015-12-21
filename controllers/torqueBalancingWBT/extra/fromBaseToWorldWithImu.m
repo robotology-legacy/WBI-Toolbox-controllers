c04769ac12d0147548722d7a92d53927a7d564c5
FirstName <daniele.pucci85@gmail.com>
2015-12-21 10:34:07 +0100

    [WBI-Toolbox-controllers] Aligned World-Imu frame and l_sole/r_sole

--- controllers/torqueBalancingWBT/extra/fromBaseToWorldWithImu.m
+++ controllers/torqueBalancingWBT/extra/fromBaseToWorldWithImu.m
@@ -1,17 +1,26 @@
-function w_H_root = fromBaseToWorldWithImu(imu_H_link,link_H_root,inertial)
+function w_H_root = fromBaseToWorldWithImu(imu_H_link,imu_H_link_0,link_H_root,inertial_0,inertial)
 %#codegen
  
 
 % See http://wiki.icub.org/images/8/82/XsensMtx.pdf page 11
 
-w_R_imu    = rotz(inertial(3))*roty(inertial(2))*rotx(inertial(1)) * rotz(-180);
+w_R_imu      = rotz(inertial(3))*roty(inertial(2))*rotx(inertial(1));
+w_R_imu_0    = rotz(inertial_0(3))*roty(inertial_0(2))*rotx(inertial_0(1));
 
-imu_R_link = imu_H_link(1:3,1:3);
+imu_R_link   = imu_H_link(1:3,1:3);
+imu_R_link_0 = imu_H_link_0(1:3,1:3);
 
 
-w_R_link   = w_R_imu*imu_R_link;
+w_R_link     = w_R_imu*imu_R_link;
+w_R_link_0   = w_R_imu_0*imu_R_link_0;
 
-w_H_link   = [w_R_link,zeros(3,1)
-              zeros(1,3),1];
 
-w_H_root   = w_H_link*link_H_root;
+w_H_link     = [w_R_link,zeros(3,1)
+                zeros(1,3),1];
+          
+w_H_link_0   = [w_R_link_0,zeros(3,1)
+                zeros(1,3),1];
+
+w_H_root     = w_H_link*link_H_root;
+
+w_H_root     = w_H_link_0\w_H_root;
