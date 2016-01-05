66ebb7e5cc3e41c3db016198f26c3d87e1e53433
FirstName <daniele.pucci85@gmail.com>
2016-01-05 23:43:11 +0100

    Added filter for getting rid of the YAW angle of IMU. Elementar rotations now take radians, and some modifications to testEstBaseImu

--- controllers/torqueBalancingWBT/extra/rotx.m
+++ controllers/torqueBalancingWBT/extra/rotx.m
@@ -1,9 +1,8 @@
-function R = rotx(angleInDegs)
-   alpha = angleInDegs / 180 * pi;
+function R = rotx(alpha)
    R = zeros(3, 3);
-   R(1,1) = 1;
-   R(2,2) = cos(alpha);
+   R(1,1) =  1;
+   R(2,2) =  cos(alpha);
    R(2,3) = -sin(alpha);
-   R(3,2) = sin(alpha);
-   R(3,3) = cos(alpha); 
+   R(3,2) =  sin(alpha);
+   R(3,3) =  cos(alpha); 
 end
--- controllers/torqueBalancingWBT/extra/rotz.m
+++ controllers/torqueBalancingWBT/extra/rotz.m
@@ -1,9 +1,8 @@
-function R = rotz(angleInDegs)
-   alpha = angleInDegs / 180 * pi;
+function R = rotz(alpha)
    R = zeros(3, 3);
-   R(3,3) = 1;
-   R(1,1) = cos(alpha);
+   R(3,3) =  1;
+   R(1,1) =  cos(alpha);
    R(1,2) = -sin(alpha);
-   R(2,1) = sin(alpha);
-   R(2,2) = cos(alpha); 
+   R(2,1) =  sin(alpha);
+   R(2,2) =  cos(alpha); 
 end
