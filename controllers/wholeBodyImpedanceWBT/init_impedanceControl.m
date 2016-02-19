d33033f15f791aaf4273b6188862c1f4bf56160d
gabrielenava <gabriele.nava@mail.polimi.it>
2016-02-19 10:41:21 +0100

    wholebodyimpedance updated to WBToolbox

---
+++ controllers/wholeBodyImpedanceWBT/init_impedanceControl.m
@@ -0,0 +1,30 @@
+%% icubGazeboSim
+
+ROBOT_DOF   = 23;
+WBT_wbiList = 'ROBOT_TORQUE_CONTROL_JOINTS_WITHOUT_PRONOSUP';
+robotName   = 'icub';
+localName   = 'impedance';
+Ts          = 0.01;
+constraints = [1;1];
+
+KpTorso   = 0.5*ones(1,3);
+KpArms    = [0.1,0.1,0.1,0.1];
+KpLegs    = [0.5,0.5,0.5,0.5,0.5,0.5];
+Kp        = diag([KpTorso,KpArms,KpArms,KpLegs,KpLegs]);
+
+if size(Kp,1) ~= ROBOT_DOF
+    error('Dimension of Kp different from ROBOT_DOF')
+end
+
+Kd        = 2*sqrt(Kp)*0;
+
+GRAV_COMP = 1;
+MOVING    = 0;
+AMPLS     = 15*ones(1,ROBOT_DOF);
+FREQS     = 0.25*ones(1,ROBOT_DOF);
+
+if MOVING == 1
+    simulationTime = inf;%5/FREQS(1);
+else
+    simulationTime = inf;
+end
