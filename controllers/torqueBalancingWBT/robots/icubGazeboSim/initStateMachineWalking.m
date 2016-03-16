945506ea321333220b97ace55b348bb3d832eeb4
Jorhabib <jorhabib@gmail.com>
2016-03-17 00:14:12 +0100

    Added check on right leg error before switching to double support

--- controllers/torqueBalancingWBT/robots/icubGazeboSim/initStateMachineWalking.m
+++ controllers/torqueBalancingWBT/robots/icubGazeboSim/initStateMachineWalking.m
@@ -6,22 +6,22 @@ if strcmpi(SM.SM_TYPE, 'WALKING')
 
     references.joints.smoothingTime    = 5;
     references.com.smoothingTime       = references.joints.smoothingTime;
-    gain.SmoothingTimeImp              = references.joints.smoothingTime;  
+    gain.SmoothingTimeImp              = 1;  
 
     smoothingTimeTransitionDynamics    = 0.02;
 
 
-    gain.PCOM              = diag([50    40  50]); 
+    gain.PCOM              = diag([50    80  50]); 
     gain.ICOM              = diag([  0    0   0]);
     gain.DCOM              = 2*sqrt(gain.PCOM);
 
     gain.PAngularMomentum  = 1 ;
 
     %                   %   TORSO  %%      LEFT ARM   %%      RIGHT ARM   %%         LEFT LEG            %%         RIGHT LEG           %% 
-    gain.impedances  = [60   60   90, 10   10    10    8, 10   10    10    8, 60   50   30    60     10  10,  60   50   30    60     10  10    % state ==  1  WATING FOR REFERENCES
-                        60   60   90, 10   10    10    8, 10   10    10    8, 60   60   60    90     90  10,  60  60   60    90     90  10    % state ==  2  TWO FEET BALANCING
+    gain.impedances  = [60   100   60, 10   10    10    8, 10   10    10    8, 60   50   30    60     10  10,  60   50   30    60     10  10    % state ==  1  WATING FOR REFERENCES
+                        60   100   60, 10   10    10    8, 10   10    10    8, 60   60   60    90     90  10,  60  60   60    90     90  10    % state ==  2  TWO FEET BALANCING
                         10   10   20, 10   10    10    8, 10   10    10    8, 90   50   20    80     30  15,  80   50   30    80     20   5    % state ==  3  LEFT FOOT BALANCING
-                        60   100   60, 10   10    10    8, 10   10    10    8, 80   50   30   100     40  25, 100   50   20  120 60  15];  % state ==  4  RIGHT FOOT BALANCING
+                        60   100   60, 30   30   30    8, 30   30    30    8, 80   50   30   100     40  25, 100   80   20  120 60  15];  % state ==  4  RIGHT FOOT BALANCING
     % Working gains when standing on right leg [130   50   20  120 60  15]
  
 
@@ -31,7 +31,7 @@ gain.dampings           = zeros(1,length(gain.impedances));
 
 %% %%%%%%%%%%%%%%%%    FINITE STATE MACHINE SPECIFIC PARAMETERS
 sm.com.threshold                 =   0.02;
-sm.wrench.threshold             = 70;
+sm.wrench.threshold             = -70;
 sm.joints.thresholdNotInContact =  3;
 sm.joints.thresholdInContact    = 110;
 
--- controllers/utilityMatlabFunctions/stateMachineWalking.m
+++ controllers/utilityMatlabFunctions/stateMachineWalking.m
@@ -1,96 +1,118 @@
 function [CoMDes,qDes,constraints, currentState,impedances,w_H_b,enableIncrement] = ...
-    stateMachineWalking(connection,CoM_0, q0, w_CoM, CoMIn, qIn, constraintsIn, wrench_rightFoot,wrench_leftFoot,l_sole_H_b, r_sole_H_b, sm,gain, time)
-    %#codegen
-    global state;
-    
-    global w_H_fixedLink;
-    global fixedLink;
+    stateMachineWalking(connection,CoM_0, q0, w_CoM, CoMIn, qIn, constraintsIn, wrench_rightFoot,wrench_leftFoot,l_sole_H_b, r_sole_H_b, sm,gain, time, qj)
+%#codegen
+global state;
+
+global w_H_fixedLink;
+global fixedLink;
 %     global counterTraj;
-    
+
 %     w_H_fixedLink  = eye(4);
-%     
+%
 %     fixedLink   =  1; % 1 = left, 2 = right
-    
+
+CoMDes      = CoM_0;
+constraints = [1; 1];
+qDes        = q0;
+
+impedances = gain.impedances(1,:);
+
+% For handling when to switch state
+enableIncrement = 1;
+qj_left_leg = qj(12:17)';
+qj_right_leg = qj(18:23)';
+
+%% STATES
+% 1 - initial state (waiting for references)
+% 2 - 2 feet CoM Tracking
+% 3 - Left foot balancing
+% 4 - Right foot balancing
+
+if fixedLink == 1
+    w_H_b   =  w_H_fixedLink * l_sole_H_b;
+else
+    w_H_b   =  w_H_fixedLink * r_sole_H_b;
+end
+
+if state == 1
+    %waiting for com reference
     CoMDes      = CoM_0;
-    constraints = [1; 1];
-    qDes        = q0;
-
-    impedances = gain.impedances(1,:);
     
-    %% STATES
-    % 1 - initial state (waiting for references)
-    % 2 - 2 feet CoM Tracking
-    % 3 - Left foot balancing
-    % 4 - Right foot balancing
+    if norm(CoM_0 - CoMIn) > eps && connection
+        state = 2;
+    end
+    enableIncrement = 1;
+elseif state == 2
     
-    if fixedLink == 1
-        w_H_b   =  w_H_fixedLink * l_sole_H_b;
-    else 
-        w_H_b   =  w_H_fixedLink * r_sole_H_b;
+    CoMDes = CoMIn;
+    qDes   = qIn;
+    impedances  = gain.impedances(state,:);
+    
+    %         CoMError  = CoMDes - w_CoM;
+    CoMError = w_CoM - CoMDes;
+    if ~any(constraintsIn - [1; 0]) ...
+            && norm(CoMError(2)) < sm.com.threshold
+        constraints = [1; 0]; %right foot is no longer a constraint
+        fixedLink = 1;
+        w_H_fixedLink = w_H_b / l_sole_H_b;
+        state = 3;
+        
+    elseif ~any(constraintsIn - [0; 1]) ...
+            && norm(CoMError(2)) < sm.com.threshold
+        constraints = [0; 1]; %left foot is no longer a constraint
+        state = 4;
+        
+        fixedLink = 2;
+        w_H_fixedLink = w_H_b / r_sole_H_b;
+        
     end
     
-    if state == 1
-        %waiting for com reference
-            CoMDes      = CoM_0;
-
-        if norm(CoM_0 - CoMIn) > eps && connection
-            state = 2;
-        end
-    elseif state == 2
-        
-        CoMDes = CoMIn;
-        qDes   = qIn;
-        impedances  = gain.impedances(state,:);
-           
-%         CoMError  = CoMDes - w_CoM;
-        CoMError = w_CoM - CoMDes;
-        if ~any(constraintsIn - [1; 0]) ...
-                && norm(CoMError(2)) < sm.com.threshold
-            constraints = [1; 0]; %right foot is no longer a constraint
-            fixedLink = 1;
-            w_H_fixedLink = w_H_b / l_sole_H_b;
-            state = 3;
-            
-        elseif ~any(constraintsIn - [0; 1]) ...
-                && norm(CoMError(2)) < sm.com.threshold
-            constraints = [0; 1]; %left foot is no longer a constraint
-            state = 4;
-            
-            fixedLink = 2;
-            w_H_fixedLink = w_H_b / r_sole_H_b;
-           
-        end
-            
-    elseif state == 3
-        % Left foot balancing
-        constraints = [1; 0];
-        
-        CoMDes = CoMIn;
-        qDes   = qIn;
-        impedances  = gain.impedances(state,:);
-        
-        if wrench_rightFoot(3) > sm.wrench.threshold ...
-                && ~any(constraintsIn - [1; 1])
-            state = 2;
-        end
-        
-    elseif state == 4
-        % Right foot balancing
-        constraints = [0; 1];
-        
-        CoMDes = CoMIn;
-        qDes   = qIn;
-        impedances  = gain.impedances(state,:);
-        
-        if wrench_leftFoot(3) > sm.wrench.threshold ...
-                && ~any(constraintsIn - [1; 1])
-            state = 2;
-        end
+elseif state == 3
+    % Left foot balancing
+    constraints = [1; 0];
+    
+    CoMDes = CoMIn;
+    qDes   = qIn;
+    impedances  = gain.impedances(state,:);
+    
+    if wrench_rightFoot(3) > sm.wrench.threshold ...
+            && ~any(constraintsIn - [1; 1])
+        state = 2;
     end
     
+elseif state == 4
+    % Right foot balancing
+    constraints = [0; 1];
+    
+    CoMDes = CoMIn;
+    qDes   = qIn;
+    qDes_right_leg = qDes(18:23)';
+    impedances  = gain.impedances(state,:);
+    
+    if ~any(constraintsIn - [1;1])
+        enableIncrement = 0;
+        
+%         if wrench_leftFoot(3) < sm.wrench.threshold ...
+%                 && norm(qj_right_leg - qDes_right_leg) < 1.5 %norma della gamba destra < threshold. Put this threshold in the configuration file for the walking state machine
+%             enableIncrement = 1;
+%             state = 2;
+%         end
+        if norm(qj_right_leg - qDes_right_leg) > 0.3
+            disp('Increasing integral gain for joint 0');
+%             gain.integral(18) = gain.integral(18) + 5;
+        end
+        if norm(qj_right_leg - qDes_right_leg) < 0.1
+            disp('NORM OF THE RIGHT LEG ERROR IS NOW LESS THAN 0.1!!!');
+            enableIncrement = 1;
+            if wrench_leftFoot(3) < sm.wrench.threshold % "<" because the threshold is negative.
+                state = 2;
+            end
+        end
+    end
+end
+
 %     counterTraj = counterTraj + 1;
 %     disp('From stateMachineWalking.m')
 %     disp('Counter Traj: '), disp(counterTraj);
 %     disp('at time: '), disp(time);
-    enableIncrement = 1;
-    currentState = state;
+currentState = state;
