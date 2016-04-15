f2bb925ddca63d498e8b08979754c808ddfe488e
FirstName <daniele.pucci85@gmail.com>
2016-04-15 15:29:26 +0200

    Cleaning the repo for the torqueBalancing on seesaw

--- controllers/torqueBalancingOnSeesaw/simulink/initTrqBalancingSeeSaw.m
+++ controllers/torqueBalancingOnSeesaw/simulink/initTrqBalancingSeeSaw.m
@@ -1,4 +1,4 @@
-clear;
+clear all;
 setenv('YARP_ROBOT_NAME','icubGazeboSim');
 
 localName               = 'seesawBalancingController';
@@ -12,6 +12,8 @@ CONFIG.USE_IMU4EST_BASE = 0;
 CONFIG.USE_IMU4EST_BASE = false;
 CONFIG.YAW_IMU_FILTER   = false;
 
+CONFIG.TS               = 0.01;
+
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CONFIGURATIONS COMPLETED: loading gains and parameters for the specific robot
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -22,3 +24,4 @@ CONFIG.ON_GAZEBO     = false;
 WBT_modelName = 'matlabTorqueBalancing';
 
 run(strcat('robots/',getenv('YARP_ROBOT_NAME'),'/gains.m')); 
+[ConstraintsMatrix,bVectorConstraints]= constraints(forceFrictionCoefficient,numberOfPoints,torsionalFrictionCoefficient,gain.footSize,fZmin);
