341e5facff67c3e0eb1f53ace4faa7b3646e842f
FirstName <daniele.pucci85@gmail.com>
2016-07-11 19:16:52 +0200

    some additional tuning for Fast YOGA++ on iCubGenova05

--- controllers/torqueBalancing/initTorqueBalancing.m
+++ controllers/torqueBalancing/initTorqueBalancing.m
@@ -29,6 +29,7 @@ clear; clc;
 % setenv('YARP_ROBOT_NAME','iCubGenova02');
 % setenv('YARP_ROBOT_NAME','iCubDarmstadt01');
 setenv('YARP_ROBOT_NAME','icubGazeboSim');
+% setenv('YARP_ROBOT_NAME','iCubGenova05');
 
 % Simulation time in seconds
 CONFIG.SIMULATION_TIME     = inf;   
