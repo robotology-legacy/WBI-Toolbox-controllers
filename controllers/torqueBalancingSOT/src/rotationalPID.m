bf5d4cd4c1053587adf62f3082fa409e8680bec1
Daniele <daniele.pucci85@gmail.com>
2017-01-22 17:42:24 +0100

    Update rotationalPID.m
    Added preliminary comments.

--- controllers/torqueBalancingSOT/src/rotationalPID.m
+++ controllers/torqueBalancingSOT/src/rotationalPID.m
@@ -1,5 +1,14 @@
 function I_desRotAcc = rotationalPID(rotMatrix,I_angVel,I_desRotMatAngVelAcc,gainsPD)
     
+    % rotMatrix = I_R_B: rotation matrix transforming a vector expressed w.r.t. a body frame B, e.g. p_B, into 
+    %                    a vector expressed in the inertial frame I, e.g. p_A, i.e.
+    %            
+    %                                   p_I = I_R_B * p_B
+    %
+    % I_angVel  = omega_I: angular velocity expressed w.r.t. the inertial frame I, i.e.
+    %
+    %                                   dot(I_R_B) = S(I_angVel) * I_R_B
+    
     B_angVel      = rotMatrix'*I_angVel;
     desRotMat     = I_desRotMatAngVelAcc(1:3,1:3);
     B_desAngVel   = desRotMat'*I_desRotMatAngVelAcc(:,4);
