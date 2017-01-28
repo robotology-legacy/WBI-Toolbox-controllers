503962ae1550a2b85d99d345f103b69a74883a9c
FirstName <daniele.pucci85@gmail.com>
2017-01-28 23:54:24 +0100

    Corrected notation and added CMM in centroidal coordinates

--- controllers/utilityMatlabFunctions/fromFloatingToCentroidalDynamics.m
+++ controllers/utilityMatlabFunctions/fromFloatingToCentroidalDynamics.m
@@ -1,44 +1,50 @@
-function [Mc,C_cNu_c, gc, Jcc, dJcDq_c, Nu_c] = fromFloatingToCentroidalDynamics(M, h, g, Jc, dJcDq, Nu, T, dT)
+function [M_c,h_c, Jc_c, Jc_cDNu_c, Nu_c,CMM,CMMc] = fromFloatingToCentroidalDynamics(M, h, g, J, dJcNu, Nu, c_T_b, dT)
 % centroidalConversion 
 % converts dynamic equation parameters to the
 % corresponding values in centroidal frame of reference
-ndof          = size(g,1)-6;
+ndof           = size(g,1)-6;
 
-invT          = eye(ndof+6)/T;
-invTt         = eye(ndof+6)/(T');
+invT           = eye(ndof+6)/c_T_b;
+invTt          = eye(ndof+6)/(c_T_b');
 
 %% control terms conversion
-Mc            = invTt*M*invT;
+M_c            = invTt*M*invT;
 
-Mc(1:6,7:end) = zeros(6,ndof);
-Mc(7:end,1:6) = zeros(ndof,6);
+M_c(1:6,7:end) = zeros(6,ndof);
+M_c(7:end,1:6) = zeros(ndof,6);
 
-Mc(1:3,1:3)   = M(1,1)*eye(3);
-Mc(1:3,4:6)   = zeros(3);
-Mc(4:6,1:3)   = zeros(3);
+M_c(1:3,1:3)   = M(1,1)*eye(3);
+M_c(1:3,4:6)   = zeros(3);
+M_c(4:6,1:3)   = zeros(3);
 
-Nu_c          = T*Nu;
+Nu_c          = c_T_b*Nu;
 gravAcc       = norm(invTt*g)/M(1,1);
 
 e3            = zeros(ndof+6,1);
 e3(3)         = 1;
-gc            = M(1,1)*gravAcc*e3;
+g_c           = M(1,1)*gravAcc*e3;
 
 CNu           = h - g;
 
 Mb            =   M(1:6,1:6);
 Mbj           =   M(1:6,7:end);
 
-C_cNu_c_dT    = invTt*CNu - Mc*dT*Nu;
+C_cNu_c_dT    = invTt*CNu - M_c*dT*Nu;
 
 C_cNu_c       = [ zeros(3,1); 
                   C_cNu_c_dT(4:6); 
                   CNu(7:end)-(Mbj')*(Mb\CNu(1:6))];
 
 %new dT*Nu computation for Jacobian
-dTNu          = Mc\(CNu-C_cNu_c);  
+dTNu          = M_c\(CNu-C_cNu_c);  
 
-Jcc           = Jc*invT;
-dJcDq_c       = dJcDq - Jc*invT*dTNu;
+Jc_c          = J*invT;
+Jc_cDNu_c     = dJcNu - Jc*invT*dTNu;
+
+CMMc          = M_c(1:6,:);
+
+CMM           = CMMc*T;
+
+h_c           = C_cNu_c + g_c;
 
 end
