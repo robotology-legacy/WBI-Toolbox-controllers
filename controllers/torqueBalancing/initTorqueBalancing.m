102107bb5fb885d01cafd5637783b539631c3370
FirstName <daniele.pucci85@gmail.com>
2017-03-15 18:42:26 +0100

    Changed variable name for Frames

--- controllers/torqueBalancing/initTorqueBalancing.m
+++ controllers/torqueBalancing/initTorqueBalancing.m
@@ -28,9 +28,9 @@ clear; clc;
 % setenv('YARP_ROBOT_NAME','iCubGenova01');
 % setenv('YARP_ROBOT_NAME','iCubGenova02');
 % setenv('YARP_ROBOT_NAME','iCubDarmstadt01');
- setenv('YARP_ROBOT_NAME','icubGazeboSim');
+% setenv('YARP_ROBOT_NAME','icubGazeboSim');
 % setenv('YARP_ROBOT_NAME','iCubGenova05');
-
+setenv('YARP_ROBOT_NAME','isaacFirstProtoGazebo');
 
 % Simulation time in seconds
 CONFIG.SIMULATION_TIME     = inf;   
@@ -75,7 +75,7 @@ CONFIG.CHECK_LIMITS        = false;
 %% CHANGED WHEN SIMULATING THE ROBOT ON GAZEBO, 
 WBT_modelName            = 'matlabTorqueBalancing';
  
-LINKS.BASE_LINK          = 'root_link'; 
+FRAMES.BASE              = 'root_link'; 
 
 
 % CONFIG.USE_IMU4EST_BASE: if set to false, the base frame is estimated by 
