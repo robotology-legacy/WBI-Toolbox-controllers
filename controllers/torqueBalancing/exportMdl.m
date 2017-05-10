7cb46203d66dbee76238dac7aeb3462e1e389a64
Gabriele <gabriele.nava@mail.polimi.it>
2017-05-10 15:43:22 +0200

    added automatic export of simulink mdl to prev. versions

---
+++ controllers/torqueBalancing/exportMdl.m
@@ -0,0 +1,74 @@
+%% EXPORTMDL 
+%  automatic exports .mdl files in previous Simulink versions for keeping 
+%  the back-compatibility of the controllers. 
+% 
+%  - available Matlab versions: 2016b, 2015b, 2015a, 2012b
+%
+%  - this script should be launched from a Matlab version greater than 2012b
+%    (either 2015a, 2015b, 2016b)
+%
+%  - adapted from https://github.com/robotology/WB-Toolbox/blob/master/toolbox/export_library.m
+%
+% Author : Gabriele Nava (gabriele.nava@iit.it)
+% Genova, April 2017
+%
+
+%% Initialization
+clc
+clear 
+close
+
+fprintf('\n### Exporting Simulink model to previous versions');
+fprintf('\n### Starting Matlab version must be one of the following: 2015a, 2015b, 2016b');
+fprintf('\n### Exported versions: 2015b, 2015a, 2012b');
+fprintf('\n### If you need to export other versions, just modify this script accordingly.\n');
+
+% at the moment, this script can export models only from a Matlab version 
+% greater than 2015a
+if (verLessThan('matlab', '8.5'))
+    error('This script should be launched with a MATLAB version >= than 2015a');
+    quit; %#ok<UNRCH>
+end
+
+%% find the Matlab version
+matlabVersion     = ver('matlab');
+matlabVersion     = matlabVersion.Version;
+% generate the prefix for loading the model
+if strcmp(matlabVersion,'9.1')
+    versionName   = 'R2016bS88';
+elseif strcmp(matlabVersion,'8.6')    
+    versionName   = 'R2015bS86';    
+elseif strcmp(matlabVersion,'8.5')    
+    versionName   = 'R2015aS85';    
+end
+modelName         = ['torqueBalancing' versionName];
+
+%% Load the Simulink model and export to previous versions
+try
+  open_system(modelName,'loadonly');
+  fprintf(['\nLOADED MODEL: ' modelName '\n']);
+
+  % export to 2012b
+  fprintf('\nExporting to version 2012b\n');
+  save_system(modelName, 'torqueBalancingR2012bS80', 'ExportToVersion', 'R2012B_MDl');
+
+  if strcmp(matlabVersion,'9.1') || strcmp(matlabVersion,'8.6')   
+      % export to 2015a
+      fprintf('\nExporting to version 2015a\n');
+      save_system(modelName, 'torqueBalancingR2015aS85', 'ExportToVersion', 'R2015A_MDl');
+
+  end 
+  if strcmp(matlabVersion,'9.1')    
+      % export to 2015b
+      fprintf('\nExporting to version 2015b\n'); 
+      save_system(modelName, 'torqueBalancingR2015bS86', 'ExportToVersion', 'R2015B_MDl');
+  end
+  
+  % closing the model
+  close_system(modelName);
+  catch ex;
+
+end
+    
+fprintf('\nDone\n');
+
