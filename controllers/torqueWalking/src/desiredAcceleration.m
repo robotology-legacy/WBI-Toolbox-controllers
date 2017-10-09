1ee7915211515cf5f8abe06b099975fe7be93480
Marie Charbonneau <marie.m.charbonneau@gmail.com>
2017-10-09 11:57:41 +0200

    Changed reference for CoM rotation

--- controllers/torqueWalking/src/desiredAcceleration.m
+++ controllers/torqueWalking/src/desiredAcceleration.m
@@ -1,6 +1,6 @@
 function [dnu_star, CoM_x_error, CoM_w_error, s_error] ...
           = desiredAcceleration(s, ds, w_H_CoM, CoMVelocity, ...
-                                desired_x_dx_ddx_CoM, desired_Intw_w_dw_CoM, desired_s_ds_dds, ...
+                                desired_x_dx_ddx_CoM, desired_R_w_dw_CoM, desired_s_ds_dds, ...
                                 gain)
 
 %CoM linear acceleration
@@ -11,7 +11,7 @@ ddx_star = linearPID(xCoM, dxCoM, desired_x_dx_ddx_CoM, [gain.x_CoM.p, gain.x_Co
 %CoM angular acceleration
 w_R_CoM     = w_H_CoM(1:3,1:3);
 w_CoM       = CoMVelocity(4:6);
-dw_star     = rotationalPID(w_R_CoM,w_CoM,desired_Intw_w_dw_CoM, [gain.w_CoM.p, gain.w_CoM.d]);
+dw_star     = rotationalPID(w_R_CoM,w_CoM,desired_R_w_dw_CoM, [gain.w_CoM.p, gain.w_CoM.d]);
 
 %Joint accelerations
 dds_star    = linearPID(s, ds, desired_s_ds_dds, [gain.joints.p, gain.joints.d]);
@@ -22,8 +22,8 @@ dnu_star    = [ddx_star; dw_star; dds_star];
 
 %Debug information
 CoM_x_error = xCoM - desired_x_dx_ddx_CoM(:,1);
-CoM_w_error = invSkew(w_R_CoM - desired_Intw_w_dw_CoM(:,1:3));
-s_error    = s - desired_s_ds_dds(:,1);
+CoM_w_error = invSkew(desired_R_w_dw_CoM(1:3,1:3)' * w_R_CoM);
+s_error     = s - desired_s_ds_dds(:,1);
 
 end
 
--- controllers/torqueWalking/src/stateMachine.m
+++ controllers/torqueWalking/src/stateMachine.m
@@ -1,14 +1,14 @@
-function [desired_x_dx_ddx_CoM, desired_Intw_w_dw_CoM, desired_s_ds_dds] = stateMachine(s0, w_H_CoM_0, ROBOT_DOF)
+function [desired_x_dx_ddx_CoM, desired_R_w_dw_CoM, desired_s_ds_dds] = stateMachine(s0, w_H_CoM_0, ROBOT_DOF)
 
 % Keep initial CoM position + a desired displacement
-CoM_displacement = [0; -0.5; 0];
+CoM_displacement = [0; -0.05; 0];
 
 % Move to the left
 desired_x_CoM = w_H_CoM_0(1:3, 4) + CoM_displacement;
 desired_x_dx_ddx_CoM = [desired_x_CoM, zeros(3,2)];
 
 % Keep initial CoM orientation
-desired_Intw_w_dw_CoM = [w_H_CoM_0(1:3,1:3), zeros(3,2)];
+desired_R_w_dw_CoM = [w_H_CoM_0(1:3,1:3), zeros(3,2)];
 
 % Keep initial joint positions
 desired_s_ds_dds = [s0, zeros(ROBOT_DOF,2)];
