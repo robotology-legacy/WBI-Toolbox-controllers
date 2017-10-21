93a1da92a73b689d72d76cbaebb5e90d21e50aba
Marie Charbonneau <marie.m.charbonneau@gmail.com>
2017-10-21 20:34:16 +0200

    Added case where one foot on the ground, in CoMaccelerationBounds

--- controllers/torqueWalking/src/CoMaccelerationBounds.m
+++ controllers/torqueWalking/src/CoMaccelerationBounds.m
@@ -1,5 +1,5 @@
 function ddx_root_bounds = CoMaccelerationBounds( w_H_root, rootVelocity, ...
-                           w_H_l_sole, w_H_r_sole, ...
+                           w_H_l_sole, w_H_r_sole, feetActivation, ...
                            gain )
 
 %% NOTE: definition of position bounds shall be redefined for walking purposes
@@ -15,15 +15,26 @@ x_l_sole = w_H_l_sole(1:2,4);
 x_r_sole = w_H_r_sole(1:2,4);
 
 %% Bounds on root position, for 2 feet balancing
-x_root_lowerBound   = min(x_r_sole, x_l_sole) + [gain.footSize(1,1); gain.footSize(2,1)];
-x_root_upperBound   = max(x_r_sole, x_l_sole) + [gain.footSize(1,2); gain.footSize(2,2)];
+if feetActivation(1) > 0.9 && feetActivation(2) > 0.9
+    x_root_lowerBound   = min(x_r_sole, x_l_sole) + [gain.footSize(1,1)*0.5; gain.footSize(2,1)*0];
+    x_root_upperBound   = max(x_r_sole, x_l_sole) + [gain.footSize(1,2); gain.footSize(2,2)*0];
+    
+elseif feetActivation(2) < 0.9 %only left foot on ground
+    x_root_lowerBound   = x_l_sole + [gain.footSize(1,1)*0.5; gain.footSize(2,1)*0.25];
+    x_root_upperBound   = x_l_sole + [gain.footSize(1,2)*0.5; gain.footSize(2,2)*0.25];
+    
+else %only right foot on ground
+    x_root_lowerBound   = x_r_sole + [gain.footSize(1,1)*0.5; gain.footSize(2,1)*0.25];
+    x_root_upperBound   = x_r_sole + [gain.footSize(1,2)*0.5; gain.footSize(2,2)*0.25];
+end
 
 ddx_root_lowerBound = gain.x_maxAcceleration * tanh( min( ...
-                          linearPID(x_root, dx_root, [x_root_lowerBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d]), ...
-                          linearPID(x_root, dx_root, [x_root_upperBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d])) );
+                      linearPID(x_root, dx_root, [x_root_lowerBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d]), ...
+                      linearPID(x_root, dx_root, [x_root_upperBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d])) );
 ddx_root_upperBound = gain.x_maxAcceleration * tanh( max( ...
-                          linearPID(x_root, dx_root, [x_root_lowerBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d]), ...
-                          linearPID(x_root, dx_root, [x_root_upperBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d])) );
+                      linearPID(x_root, dx_root, [x_root_lowerBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d]), ...
+                      linearPID(x_root, dx_root, [x_root_upperBound zeros(2,2)], [gain.x_rootbound.p, gain.x_rootbound.d])) );
+
 
 ddx_root_bounds     = [ddx_root_lowerBound; ddx_root_upperBound];
 
